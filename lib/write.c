b90371da082a68162855ad613b86b3ca8d5c93fc
sheldon.liu <ljb861213@163.com>
2016-09-09 16:44:15 +0800

    update

--- lib/write.c
+++ lib/write.c
@@ -7,4 +7,22 @@
 #define __LIBRARY__
 #include <unistd.h>
 
+#if 0
+
 _syscall3(int,write,int,fd,const char *,buf,off_t,count)
+
+#else
+int write(int fd,const char * buf,off_t count)
+{
+    long __res; 
+    __asm__ volatile ("int $0x80" 
+    	: "=a" (__res) 
+    	: "0" (__NR_write),"b" ((long)(fd)),"c" ((long)(buf)),"d" ((long)(count))); 
+    if (__res>=0) 
+    	return (int) __res; 
+    errno=-__res; 
+    return -1; 
+}
+#endif
+
+
